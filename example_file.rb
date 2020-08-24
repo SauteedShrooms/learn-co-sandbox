a4a4db16fb02225679a8b8808b58390ab7c28265
Nicholas Baccete <nickbaccete@gmail.com>
2020-08-24 18:01:49 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,17 @@
+def a_method(a, b)
+  puts "i got #{a}"
+  puts "i got #{b}"
+  sum = a + b
+  puts "i got #{sum}"
+  sum
+end
+
+
+a_method(2, 3)
+
+
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "martha Stewart"
+  "Guy Fieri"
+end
---
+++ greeting.rb
@@ -0,0 +1,13 @@
+def greeting
+  puts "Hello World!"
+end
+
+def say_greeeting_five_times
+greeting
+greeting
+greeting
+greeting
+greeting
+end
+
+say_greeeting_five_times
